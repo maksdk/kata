545e86f53ec8e17674189cd245fcd886ed83ac89
maksdk <havryshchenko@gmail.com>
2019-12-16 16:42:15 +0200

    tree of dependencies

---
+++ challenges/dependencies/sortDepth2.js
@@ -0,0 +1,13 @@
+export default (deps) => {
+    const add = (acc, node) => {
+        const subDeps = deps[node] || [];
+        const subAcc = subDeps.reduce(add, []);
+        return {
+            ...acc,
+            ...subAcc,
+            [node]: true
+        };
+    };
+    const set = Object.keys(deps).reduce(add, {});
+    return Object.keys(set);
+};
