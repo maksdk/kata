d5ff35ceb77f2412100b46fb027498702f9d1858
maksdk <havryshchenko@gmail.com>
2020-01-08 11:15:04 +0200

    state's proccesses

--- 03/src/index.js
+++ 03/src/index.js
@@ -1,73 +1,101 @@
-//@ts-check
+// @ts-check
+/* eslint-disable no-param-reassign */
 
-const createForm = (name, value) => {
-    const form = document.createElement("form");
-
-    const inputText = document.createElement("input");
-    inputText.setAttribute("type", "text");
-    inputText.setAttribute("name", name);
-    inputText.setAttribute("value", value);
-    form.appendChild(inputText);
-    //@ts-ignore
-    inputText.focus();
-    //@ts-ignore
-    inputText.select();
-
-    const inputsubmit = document.createElement("input");
-    inputsubmit.setAttribute("type", "submit");
-    inputsubmit.setAttribute("value", "Save");
-    form.appendChild(inputsubmit);
-
-    return form;
-};
-
-const render = (state) => {
-    const { currentTarget, currentState, targets } = state;
-
-    if (currentState === "focus") {
-        const { value } = targets[currentTarget];
-        const target = document.querySelector(`[data-editable-target=${currentTarget}]`);
-        [...target.children].forEach(child => child.remove());
-        target.appendChild(createForm(currentTarget, value));
+// BEGIN (write your solution here)
+function getForm(type) {
+    return [...document.forms].find(form => {
+      const { elements } = form;
+      if (elements[type]) return true;
+      return false;
+    });
+  }
+  
+  function setTargetEventByName(name, event, cb) {
+    const target = document.querySelector(`[data-editable-target=${name}]`);
+    target.addEventListener(event, cb, false);
+  }
+  
+  const render = (state) => {
+    const { currentTarget } = state;
+  
+    if (state[currentTarget].state === "focus") {
+      const { currentValue } = state[currentTarget];
+      const template = `<form><input type=text name=${currentTarget} value=${currentValue}><input type=submit value=Save></form>`;
+  
+      const target = document.querySelector(`[data-editable-target=${currentTarget}]`);
+      target.innerHTML = template;
     }
-};
-
-const app = () => {
+  
+    if (state[currentTarget].state === "submit") {
+      const { currentValue, defaultValue } = state[currentTarget];
+      const html = currentValue 
+        ? `${currentValue}`
+        : `<i>${defaultValue}</i>`;
+  
+      const target = document.querySelector(`[data-editable-target=${currentTarget}]`);
+      target.innerHTML = html;
+    }
+  };
+  
+  const app = () => {
     const state = { 
-        currentTarget: null,
-        currentState: "idle",
-        targets: {
-            name: {
-                value: "name"
-            },
-            email: {
-                value: "email"
-            }
-        }
+      currentTarget: null,
+      name: {
+        state: "idle",
+        defaultValue: "name",
+        currentValue: ""
+      },
+      email: {
+        state: "idle",
+        defaultValue: "email",
+        currentValue: ""
+      }
     };
-
-    const name = document.querySelector('[data-editable-target="name"]');
-    const email = document.querySelector('[data-editable-target="email"]'); 
-
+  
+    const handleOnSubmit = (event, targetType) => {
+      const input = document.querySelector(`input[name=${targetType}]`);
+      const value = input.getAttribute("value");
+  
+      state.currentTarget = targetType;
+      state[targetType].state = "submit";
+      state[targetType].currentValue = value;
+  
+      setTargetEventByName(targetType, "click", handleOnClick);
+      
+      render(state);
+  
+      event.preventDefault();
+    };
+      
     const handleOnClick = (e) => {
-        const { currentTarget } = e;
-        const { targets } = state;
-        const currentTargetType = currentTarget.getAttribute("data-editable-target");
-
-        if (targets[currentTargetType]) {
-            state.currentTarget = currentTargetType;
-            state.currentState = "focus";
-        } 
-
-        render(state);
+      const { currentTarget } = e;
+      const currentTargetType = currentTarget.getAttribute("data-editable-target");
+  
+      state[currentTargetType].state = "focus";
+      state.currentTarget = currentTargetType;
+  
+      render(state);
+      
+      const form = getForm(currentTargetType);
+      if (form) {
+        const input = document.querySelector(`input[name=${currentTargetType}]`);
+        //@ts-ignore
+        input.focus();
+        //@ts-ignore
+        input.select();
+  
+        form.addEventListener("submit", (e) => handleOnSubmit(e, currentTargetType), false);
+      }
+  
+      currentTarget.removeEventListener("click", handleOnClick);
     };
-
-    name.addEventListener("click", handleOnClick, false);
-    email.addEventListener("click", handleOnClick, false);
-};
-
-window.addEventListener("load", () => {
-    app();
-});
-
-// export default app;
+  
+    ["name", "email"]
+      .forEach(name => 
+        setTargetEventByName(name, "click", handleOnClick)
+      );
+  };
+  
+  export default app;
+  // END
+  
