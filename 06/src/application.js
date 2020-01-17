12f4867a179182a2ce643f763dab022aa757f06c
maksdk <havryshchenko@gmail.com>
2020-01-17 16:31:05 +0200

    solution 1

--- 06/src/application.js
+++ 06/src/application.js
@@ -1,26 +1,24 @@
-// @ts-check
+//@ts-check
 // import 'core-js/stable';
 // import 'regenerator-runtime/runtime';
 // import _ from 'lodash';
 
-// import {
-//     watch
-// } from 'melanke-watchjs';
-// import axios from 'axios';
+// import { watch } from 'melanke-watchjs';
+import axios from "axios";
 
 // Never hardcore urls
 const routes = {
-    usersPath: () => '/users',
+  usersPath: () => '/users',
 }
 
 const errorMessages = {
-    email: {
-        valid: 'Value is not a valid email',
-    },
-    password: {
-        length: 'Must be at least 6 letters',
-        match: 'Password confirmation does not match to password',
-    },
+  email: {
+    valid: 'Value is not a valid email',
+  },
+  password: {
+    length: 'Must be at least 6 letters',
+    match: 'Password confirmation does not match to password',
+  },
 };
 
 // BEGIN (write your solution here)
@@ -35,26 +33,72 @@ const insertAfter = (newNode, referenceNode) => {
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 };
 
-const mapValidates = {
-    "name": (value) => {
-        if (value === "") return { state: "empty" };
-        return { state: "valid" }; 
+const setStates = {
+    "name": (value, state) => {
+        state.name.value = value;
+        state.name.error = "";
+
+        if (value === "") {
+            state.name.state = "empty";
+        }
+        else {
+           state.name.state = "valid"; 
+        }
     },
-    "email": (value) => {
-        if (!value) return { state: 'empty' };
-        if (!/\S+@\S+\.\S+/.test(value)) return { state: "invalid", error: errorMessages.email.valid }; 
-        return { state: "valid" };
+    "email": (value, state) => {
+        state.email.value = value;
+        state.email.error = "";
+
+        if (!value) {
+          state.email.state = 'empty';
+        }
+        else if (!/\S+@\S+\.\S+/.test(value)){ 
+          state.email.state =  "invalid", 
+          state.email.error = errorMessages.email.valid 
+        }
+        else {
+          state.email.state =  "valid";
+        }
     },
-    "password": (value) => {
-        if (!value) return { state: 'empty' };
-        if (value.length < 6) return { state: "invalid", error: errorMessages.password.length };
-        return { state: "valid" };
+    "password": (value, state) => {
+        state.password.value = value;
+        state.password.error = "";
+
+        if (!value) { 
+          state.password.state = 'empty';
+        }
+        else if (value.length < 6) { 
+          state.password.state = "invalid";
+          state.password.error = errorMessages.password.length 
+        }
+        else if (value !== state.passwordConfirmation.value) { 
+          state.password.state = "invalid"; 
+          state.password.error = errorMessages.password.match 
+        }
+        else {
+          state.password.state = "valid"; 
+        }
     },
-    "passwordConfirmation": (value, globalState) => {
-        const { password } = globalState;
-        if (!value) return { state: "empty" };
-        if (value !== password.value) return { state: "invalid", error: errorMessages.password.match };
-        return { state: "valid" };
+    "passwordConfirmation": (value, state) => {
+        state.passwordConfirmation.value = value;
+        state.passwordConfirmation.error = "";
+
+        if (state.password.state === "invalid") return;
+
+        if (!value) { 
+          state.passwordConfirmation.state = 'empty';
+        }
+        else if (value.length < 6) { 
+          state.passwordConfirmation.state = "invalid";
+          state.passwordConfirmation.error = errorMessages.password.length 
+        }
+        else if (value !== state.password.value) { 
+          state.passwordConfirmation.state = "invalid"; 
+          state.passwordConfirmation.error = errorMessages.password.match 
+        }
+        else {
+          state.passwordConfirmation.state = "valid"; 
+        }
     }
 };
 
@@ -76,30 +120,54 @@ const render = (state, target) => {
     }
     else {
         target.classList.remove("is-invalid");
-    }
+    } 
+};
 
-    const submit = target.form.elements[target.form.elements.length - 1];
+const renderForm = (state, form) => {
+    const submit = form.elements[form.elements.length - 1];
     if (state.submit.state === "able") {
         submit.removeAttribute("disabled");
     }
-    else {
-        submit.setAttribute("disabled", true);
+    else if (state.submit.state === "disabled") {
+        submit.setAttribute("disabled", "");
     }
-};
+    else if (state.submit.state === "sending") {
+        submit.setAttribute("disabled", "");
+        form.reset();
 
-const handleSubmit = (event, state) => {
-    console.log("add logic for submit")
+        const newForm = form.cloneNode(true);
+        form.parentNode.replaceChild(newForm, form);
+    }
+    else if (state.submit.state === "sent") {
+        const container = document.querySelector('[data-container="sign-up"]');
+        container.innerHTML = "";
+        container.appendChild(document.createTextNode("User Created!"));
+    }
+}
+
+const handleSubmit = async (event, state) => {
+    event.preventDefault();
+    
+    state.submit.state = "sending";
+    renderForm(state, event.target);
+    
+    const resullt = await axios({
+        method: 'post',
+        url: '/users',
+        data: {},
+        headers: {'Content-Type': 'multipart/form-data' }
+    });
+
+    state.submit.state = "sent";
+    renderForm(state, event.target);
+    
 };  
 
 const handleInput = (event, state) => {
     const { target } = event;
     const { name, value } = target;
     
-    const { state: elemState, error="" } = mapValidates[name](value, state);
-
-    state[name].state = elemState;
-    state[name].value = value;
-    state[name].error = error;
+    setStates[name](value, state);
 
     const checkValidForm = (state) => {
         for (let key in state) {
@@ -108,14 +176,15 @@ const handleInput = (event, state) => {
         return true;
     };
 
+    render(state, target);
+
     if (checkValidForm(state)) {
         state.submit.state = "able";
     }
     else {
         state.submit.state = "disabled";
     }
-
-    render(state, target);
+    renderForm(state, target.form);
 };  
 
 const app = () => {
@@ -149,6 +218,6 @@ const app = () => {
     form.addEventListener("input", (e) => handleInput(e, state), false);
     form.addEventListener("submit", (e) => handleSubmit(e, state), false);
 };
-app()
-// export default app;
+
+export default app;
 // END
