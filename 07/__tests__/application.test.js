1ce129d70fff5d850d3011f8b556ce22d41ed3c5
maksdk <havryshchenko@gmail.com>
2020-01-19 10:26:05 +0200

    init task

---
+++ 07/__tests__/application.test.js
@@ -0,0 +1,52 @@
+import fs from 'fs';
+import path from 'path';
+import {
+    html
+} from 'js-beautify';
+import timer from 'timer-promise';
+
+import run from '../src/application';
+
+const htmlOptions = {
+    preserve_newlines: true,
+    unformatted: [],
+};
+
+const fixturesPath = path.join(__dirname, '__fixtures__');
+const getTree = () => html(document.body.innerHTML, htmlOptions);
+
+beforeEach(() => {
+    const initHtml = fs.readFileSync(path.join(fixturesPath, 'index.html')).toString();
+    document.documentElement.innerHTML = initHtml;
+    run();
+});
+
+test('application', async () => {
+    expect(getTree()).toMatchSnapshot();
+
+    const [nameLink, valueLink] = document.querySelectorAll('a');
+
+    nameLink.click();
+    await timer.start(10);
+    expect(getTree()).toMatchSnapshot();
+
+    valueLink.click();
+    await timer.start(10);
+    expect(getTree()).toMatchSnapshot();
+
+    valueLink.click();
+    await timer.start(10);
+    expect(getTree()).toMatchSnapshot();
+
+    nameLink.click();
+    await timer.start(10);
+    expect(getTree()).toMatchSnapshot();
+
+    valueLink.click();
+    await timer.start(10);
+    expect(getTree()).toMatchSnapshot();
+});
+
+test('application 2', async () => {
+    expect(getTree()).toMatchSnapshot();
+});
---
+++ 07/src/application.js
@@ -0,0 +1,9 @@
+// @ts-check
+
+import i18next from 'i18next';
+import { watch } from 'melanke-watchjs';
+import resources from './locales';
+
+// BEGIN (write your solution here)
+
+// END
