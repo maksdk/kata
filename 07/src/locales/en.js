94d127433b450f77c3923a35693d3fc518f1bd81
maksdk <havryshchenko@gmail.com>
2020-01-20 13:50:51 +0200

    solution 2

--- 07/src/locales/en.js
+++ 07/src/locales/en.js
@@ -1,1 +1,14 @@
 //@ts-check
+export default {
+    translation: {
+        unsorted: 'Unsorted',
+        asc: 'Asc',
+        desc: 'Desc',
+        grid: {
+            cols: {
+                name: 'Name ({{ direction }})',
+                value: 'Value ({{ direction }})',
+            }
+        }
+    }
+}
