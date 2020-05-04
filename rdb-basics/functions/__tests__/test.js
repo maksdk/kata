4a564cf6b04a92afebffcd5cd9b5f85e63df6064
maksdk <havryshchenko@gmail.com>
2020-05-04 07:32:27 +0300

    functions

---
+++ rdb-basics/functions/__tests__/test.js
@@ -0,0 +1,36 @@
+// import _ from 'lodash';
+import { promises as fs } from 'fs';
+import knex from 'knex';
+
+let client;
+
+beforeAll(async () => {
+  const connection = {
+    host: '/var/run/postgresql',
+  };
+  client = knex({ client: 'pg', connection });
+  // client.on('query', console.log);
+});
+
+test('solution', async () => {
+  const sql = await fs.readFile(`${__dirname}/../solution.sql`, 'utf-8');
+  try {
+    await client.transaction(async (trx) => {
+      await trx.raw(sql);
+
+      const result = await trx.raw(sql);
+      const expected = [{ count: '4' }];
+      expect(result.rows).toEqual(expected);
+
+      throw new Error('ignore');
+    });
+  } catch (e) {
+    if (!e.toString().match(/ignore/)) {
+      throw e;
+    }
+  }
+});
+
+afterAll(async () => {
+  await client.destroy();
+});
