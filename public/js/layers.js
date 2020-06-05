d2f2c1897eb37f382effda7b7e62efda64833777
maximgavrischenko <maxim.gavrischenko@gismart.com>
2020-06-05 17:25:58 +0300

    Vector / Entity / refactored

--- public/js/layers.js
+++ public/js/layers.js
@@ -25,8 +25,8 @@ export function createBackgroundLayer(backgrounds, sprites) {
     }
 }
 
-export function createSpriteLayer(sprite, pos) {
+export function createSpriteLayer(entity) {
     return function drawSpriteLayer(context) {
-        sprite.draw('idle', context, pos.x, pos.y);
+        entity.draw(context);
     }
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,35 +1,36 @@
 // @ts-check
 import Compositor from './Compositor.js';
+import { createMario } from './entities.js';
 import { loadLevel } from './loaders.js';
-import { loadMarioSprite, loadBackgroundSprites } from './sprites.js';
+import { loadBackgroundSprites } from './sprites.js';
 import { createSpriteLayer, createBackgroundLayer } from './layers.js';
+
 
 const canvas = document.getElementById('screen');
 /** @type {CanvasRenderingContext2D} */
 // @ts-ignore
 const context = canvas.getContext('2d');
 
-Promise.all([loadMarioSprite(), loadBackgroundSprites(), loadLevel('1-1')])
-    .then(([marioSprite, backgroundSprites, level]) => {
+Promise.all([createMario(), loadBackgroundSprites(), loadLevel('1-1')])
+    .then(([mario, backgroundSprites, level]) => {
         const comp = new Compositor();
 
         const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
         comp.layers.push(backgroundLayer);
 
-        const pos = {
-            x: 64,
-            y: 64
-        };
-        const marioLayer = createSpriteLayer(marioSprite, pos);
-        comp.layers.push(marioLayer);
+        const gravity = 0.5;
+
+        const spriteLayer = createSpriteLayer(mario);
+        comp.layers.push(spriteLayer);
 
         function update() {
             comp.draw(context);
-            pos.x += 2;
-            pos.y += 2;
+            mario.update();
+
+            mario.vel.y += gravity;
+
             requestAnimationFrame(update);
         }
 
         update();
-
     });
