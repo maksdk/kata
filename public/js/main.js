6447c94a869819beb9dfc6c7b107a4562da23756
maximgavrischenko <maxim.gavrischenko@gismart.com>
2020-06-06 09:40:11 +0300

    keyboard

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,11 @@
 // @ts-check
 import Compositor from './Compositor.js';
 import Timer from './Timer.js';
+import Keyboard from './KeyboardState.js';
 import { createMario } from './entities.js';
 import { loadLevel } from './loaders.js';
 import { loadBackgroundSprites } from './sprites.js';
 import { createSpriteLayer, createBackgroundLayer } from './layers.js';
-
 
 const canvas = document.getElementById('screen');
 /** @type {CanvasRenderingContext2D} */
@@ -14,12 +14,24 @@ const context = canvas.getContext('2d');
 
 Promise.all([createMario(), loadBackgroundSprites(), loadLevel('1-1')])
     .then(([mario, backgroundSprites, level]) => {
+        
+
         const comp = new Compositor();
 
         const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
         comp.layers.push(backgroundLayer);
 
-        const gravity = 30;
+        const SPACE = 32;
+        const input = new Keyboard();
+        input.listenTo(window);
+        input.addMapping(SPACE, (keyState) => {
+            console.log(keyState);
+            if (keyState) {
+                mario.jump.start();
+            }
+        });
+
+        const gravity = 2000;
         mario.pos.set(64, 180);
         mario.vel.set(200, -600);
 
@@ -30,7 +42,7 @@ Promise.all([createMario(), loadBackgroundSprites(), loadLevel('1-1')])
         timer.update = function update(deltaTime) {
             comp.draw(context);
             mario.update(deltaTime);
-            mario.vel.y += gravity;
+            mario.vel.y += gravity * deltaTime;
         }
 
         timer.start();
