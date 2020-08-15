6f6a487612fd1c27a0c70da86f178cb33b036e66
maksdk <havryshchenko@gmail.com>
2020-08-15 20:41:37 +0300

    Draw grid

--- petty-game/src/game.js
+++ petty-game/src/game.js
@@ -1,28 +1,67 @@
 // @ts-check
 /* eslint-disable */
 import {
-  Application, Container, Graphics
+  Application, Container, Graphics, Text
 } from 'pixi.js';
 
-const CONFIG = {
+const config = {
   width: 1280,
   height: 720,
   scale: 1,
   w: 0,
   h: 0,
-  backgroundColor: 0xFF0000
+  backgroundColor: 0x607d8b
 };
+
+const cellH = 40
+const cellW = 40
+const gridWidth = config.width / cellH;
+const gridHeight = config.height / cellW;
+const gridLength = gridWidth * gridHeight;
+
+function getIndex(x, y, w) {
+  const index = w * y + x;
+  return index;
+}
+
+function getPoint(index, w, h) {
+  const y = Math.floor(index / w);
+  const x = index - (y * w);
+  return [ x, y ]; 
+}
 
 class MainWindow extends Container {
   constructor() {
     super();
   
-    const rect = new Graphics();
-    rect.beginFill(0xFFFFFF);
-    rect.drawRect(CONFIG.width * -0.5, CONFIG.height * -0.5, CONFIG.width, CONFIG.height);
-    rect.endFill();
+    const back = new Graphics();
+    back.beginFill(0xFFFFFF);
+    back.drawRect(config.width * -0.5, config.height * -0.5, config.width, config.height);
+    back.endFill();
+    this.addChild(back);
 
-    this.addChild(rect);
+    const field = new Container();
+    field.x = config.width * -0.5;
+    field.y = config.height * -0.5;
+    this.addChild(field);
+
+    for (let i = 0; i < gridLength; i += 1) {
+      const cell = new Graphics();
+      cell.lineStyle(1, 0xFF0000);
+      cell.drawRect(0, 0, cellW, cellH);
+      cell.endFill();
+      field.addChild(cell);
+
+      // const text = new Text(String(i), { fontSize: 10 });
+      // text.anchor.set(0.5);
+      // text.x = cellW * 0.5;
+      // text.y = cellH * 0.5;
+      // cell.addChild(text);
+
+      const [x, y] = getPoint(i, gridWidth, gridHeight);
+      cell.x = x * cellW;
+      cell.y = y * cellH;
+    }
   }
 }
 
@@ -37,7 +76,7 @@ class Game {
   }
 
   create() {
-    this.app = new Application(CONFIG);
+    this.app = new Application(config);
     this.app.view.style.position = 'absolute';
     this.app.view.style.left = '0';
     this.app.view.style.top = '0';
@@ -73,20 +112,20 @@ class Game {
     let gh;
 
     if (isLand) {
-      gh = CONFIG.width;
+      gh = config.width;
       gw = Math.floor(gh * (w / h));
   
-      if (gw < CONFIG.height) {
-        gw = CONFIG.height;
-        gh = Math.floor(CONFIG.height * (h / w));
+      if (gw < config.height) {
+        gw = config.height;
+        gh = Math.floor(config.height * (h / w));
       }
     } else {
-      gh = CONFIG.height;
+      gh = config.height;
       gw = Math.floor(gh * (w / h));
   
-      if (gw < CONFIG.width) {
-        gw = CONFIG.width;
-        gh = Math.floor(CONFIG.width * (h / w));
+      if (gw < config.width) {
+        gw = config.width;
+        gh = Math.floor(config.width * (h / w));
       }
     }
 
