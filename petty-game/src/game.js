05149796267b73e16e0602b0da9677a7a434ec0c
maksdk <havryshchenko@gmail.com>
2020-08-29 22:21:57 +0300

    Added editor

--- petty-game/src/game.js
+++ petty-game/src/game.js
@@ -1,11 +1,10 @@
 // @ts-check
 /* eslint-disable */
 import {
-  Application, Container, Graphics, Text, utils
+  Application, Container, Graphics, Text, utils, Sprite
 } from 'pixi.js';
-
-import has from 'lodash/has';
-import FontFaceObserver from 'fontfaceobserver';
+import range from 'lodash/range';
+import chunk from 'lodash/chunk';
 
 const config = {
   width: 1280,
@@ -16,8 +15,8 @@ const config = {
   backgroundColor: 0x607d8b
 };
 
-const cellH = 40
-const cellW = 40
+const cellH = 20;
+const cellW = 20;
 const gridWidth = config.width / cellH;
 const gridHeight = config.height / cellW;
 const gridLength = gridWidth * gridHeight;
@@ -33,103 +32,140 @@ function getPoint(index, w, h) {
   return [ x, y ]; 
 }
 
-export class AssetsPreloader {
-  constructor(app, assets) {
-    this.options = { crossOrigin: 'anonymous' }
-    this.app = app;
-    this.assets = {
-      images: [
-        { name: 'cartman', url: 'assets/images/cartman.png' },
-        { name: 'butters', url: 'assets/images/butters.png' },
-        { name: 'kenny', url: 'assets/images/kenny.png' }
-      ],
-      fonts: [
-        { name: 'MotionPicture', url: 'assets/fonts/MotionPicture.ttf' },
-        { name: 'Coneria', url: 'assets/fonts/Coneria.ttf' }
-      ]
-    };
+class Cell extends Container {
+    constructor(w, h, index, state) {
+        super();
 
-    this.loadedItemsCount = 0;
-    this.totalItemsCount = Object.values(this.assets)
-      .reduce((acc, item) => acc + item.length, 0);
+        this.index = index;
 
-    this.loadersMap = {
-      images: (loader) => loader.loadImages(),
-      fonts: (loader) => loader.loadFonts()
-    };
+        this.state = state;
 
-    Object.keys(this.assets)
-      .forEach((key) => this.loadersMap[key](this));
-  }
+        this.back = new Graphics();
+        this.back.beginFill(0xFFFFFF);
+        this.back.drawRect(0, 0, w, h);
+        this.back.endFill();
+        this.addChild(this.back);
 
-  loadImages(images = this.assets.images) {
-    images.forEach(({ name, url }) => {
-      if (!has(this.app.loader.resources, name)) {
-        this.app.loader.add(name, url, this.options);
-      }
-    });
+        const border = new Graphics();
+        border.lineStyle(1, 0x000000);
+        border.drawRect(0, 0, w, h);
+        border.endFill();
+        this.addChild(border);
 
-    this.app.loader.onProgress.add(() => this.incrementProgress());
+        this[state](this.state);
+    }
 
-    return new Promise((resolve) => this.app.loader.load(resolve));
-  }
+    fill(state) {
+        this.state = state;
+        this.back.tint = 0x666666;
+    }
 
-  loadFonts(fonts = this.assets.fonts) {
-		const [container] = document.getElementsByTagName('head');
-		const promises = fonts.map(({ name, url }) => {
-			const style = document.createElement('style');
-			style.type = 'text/css';
-			style.appendChild(document.createTextNode(`@font-face { font-family:${name}; src: url(${url}); }`));
-			container.appendChild(style);
+    clear(state) {
+        this.state = state;
+        this.back.tint = 0xFFFFFF;
+    }
+}
 
-			return new FontFaceObserver(name).load()
-				.then(() => this.incrementProgress());
-		});
+class EventShape extends utils.EventEmitter {
+    constructor(target) {
+        super();
 
-		return Promise.all(promises);
-  }
-  
-  incrementProgress() {
-    this.loadedItemsCount += 1;
-    console.log('progress: ', this.loadedItemsCount,Math.floor((this.loadedItemsCount / this.totalItemsCount) * 100) );
-		// if (this.onProgressCallback) {
-		// 	this.onProgressCallback(Math.floor((this.loadedItemsCount / this.totalItemsCount) * 100));
-		// }
-	}
+        this.target = target;
+        this.pointerState = 'none';
+        
+        target.interactive = true;
+
+        target.on('pointerdown', this.onPointerDown, this);
+        target.on('pointerup', this.onPointerUp, this);
+        target.on('pointerupoutside', this.onPointerUp, this);
+        target.on('pointerout', this.onPointerUp, this);
+        target.on('pointermove', this.onPointerMove, this);
+    }
+
+    onPointerDown(e) {
+        if (this.pointerState === 'none') {
+            this.pointerState = 'down';
+            this.emit('eventShape:down', e, this.target);
+        }
+    }
+
+    onPointerUp(e) {
+        if (this.pointerState === 'down' || this.pointerState === 'move') {
+            this.pointerState = 'none';
+            this.emit('eventShape:up', e, this.target);
+        }
+    }
+
+    onPointerMove(e) {
+        if (this.pointerState === 'down' || this.pointerState === 'move') {
+            this.pointerState = 'move';
+            this.emit('eventShape:move', e, this.target);
+        }
+    }
 }
 
 class MainWindow extends Container {
   constructor() {
     super();
-  
-    const back = new Graphics();
-    back.beginFill(0xFFFFFF);
-    back.drawRect(config.width * -0.5, config.height * -0.5, config.width, config.height);
-    back.endFill();
-    this.addChild(back);
+
+    let editState = 'fill';
+
+    const board = new Graphics();
+    board.beginFill(0xFFFFFF);
+    board.drawRect(0, 0, config.width, config.height);
+    board.endFill();
+    board.x = config.width * -0.5;
+    board.y = config.height * -0.5;
+    this.addChild(board);
+
 
     const world = new Container();
     world.x = config.width * -0.5;
     world.y = config.height * -0.5;
     this.addChild(world);
 
-    for (let i = 0; i < gridLength; i += 1) {
-      const cell = new Graphics();
-      cell.lineStyle(1, 0xFF0000);
-      cell.drawRect(0, 0, cellW, cellH);
-      cell.endFill();
-      world.addChild(cell);
+    const cells = range(gridLength).map((index) => {
+        const [x, y] = getPoint(index, gridWidth, gridHeight);
 
-      // const text = new Text(String(i), { fontSize: 10 });
-      // text.anchor.set(0.5);
-      // text.x = cellW * 0.5;
-      // text.y = cellH * 0.5;
-      // cell.addChild(text);
+        const cell = new Cell(cellW, cellH, index, editState);
+        cell.x = x * cellW;
+        cell.y = y * cellH;
+        world.addChild(cell);
 
-      const [x, y] = getPoint(i, gridWidth, gridHeight);
-      cell.x = x * cellW;
-      cell.y = y * cellH;
-    }
+        return cell;
+    });
+
+    const eventShape = new EventShape(board);
+    eventShape.on('eventShape:down', (e) => {
+        const pos = e.data.getLocalPosition(board);
+        const x = Math.floor(pos.x / cellH);
+        const y = Math.floor(pos.y / cellH);
+        const index = getIndex(x, y, gridWidth);
+        const cell = cells[index];
+
+        if (editState === 'fill' && editState === cell.state) {
+            editState = 'clear';
+        } else if (editState === 'clear' && editState === cell.state) {
+            editState = 'fill';
+        }
+
+        cell[editState](editState);
+    });
+    eventShape.on('eventShape:up', (e) => {
+        // const result = cells.map((cell) => {
+        //     if (cell.state === 'fill') return 1;
+        //     return 0;
+        // });
+        // console.table(chunk(result, gridWidth));
+    });
+    eventShape.on('eventShape:move', (e) => {
+        const pos = e.data.getLocalPosition(board);
+        const x = Math.floor(pos.x / cellH);
+        const y = Math.floor(pos.y / cellH);
+        const index = getIndex(x, y, gridWidth);
+        const cell = cells[index];
+        cell[editState](editState);
+    });
   }
 }
 
@@ -159,9 +195,6 @@ class Game {
     this.currentWindow.position.set(this.app.renderer.width / 2, this.app.renderer.height / 2);
 
     this.onResize();
-
-
-    const loader = new AssetsPreloader(this.app, []);
   }
 
   onResize() {
